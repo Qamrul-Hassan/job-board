d725c2405ec8d536704e8f6f82756960db814714
Qamrul-Hassan <sajal700@gmail.com>
2026-01-31 02:45:15 +0600

    adde axios

--- lib/api.ts
+++ lib/api.ts
@@ -1,11 +1,10 @@
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: "https://remotive.com/api",
+});
+
 export async function getJobs() {
-  const res = await fetch(
-    "https://remotive.com/api/remote-jobs"
-  );
-
-  if (!res.ok) {
-    throw new Error("Failed to fetch jobs");
-  }
-
-  return res.json(); // MUST return { jobs: [] }
+  const res = await api.get("/remote-jobs");
+  return res.data;
 }
